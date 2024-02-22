47e0b1c343b89c2d10be5c4d2f262e2429a206b8
Bobblen <r.hill147@gmail.com>
2024-02-22 16:58:14 +0000

    add map reader and builder scripts, add readme

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# avpjag_geom_extract
-Alien versus Predator Jaguar map reader php script &amp; geometry creator Blender script
+# Alien versus Predator Jaguar Geometry Extraction PHP Helper Script
+This PHP script will read a binary dump of an Alien vs Predator jaguar level map and generate a text file of the map documented as a simple list of single letter codes. 
+This text file can be read by the avpjag_mapbuilder.py script to generate a 3d mesh of the level in Blender.
+
+This script is very bare bones, and has had little testing. Use at your own risk.
+
+## Credits
+This script is based on reverse engineering of Alien versus Predator by Rich Whitehouse, as well as my own research on the map format performed using the BigPEmu Jaguar emulator and Noesis live debugger also by Rich Whitehouse
+(https://www.richwhitehouse.com/jaguar/)
+
+
+## Usage
+The script almost works as is, but needs to be modified a little to suit your particular setup.
+
+The $type variable at the top of the script can only be set to 'textfile' so can be left alone.
+
+The $filename variable at the top of the script should be set to the name of the file you're interested in. This should be a binary dump of an Avp Jag level map.
+
+This line '$bin = fopen("D:\\Maps\\".$filename, "r");' should be modified to reflect the location of the file you're interested in. Ideally that would be a variable too, but isn't yet.
+
+Binary dumps of avp map data can be created using BigPEmu Debug version combined with Noesis Debugger. 
+Start a game on the level you want, and use Noesis to dump memory from offset 0x4FC00. The amount of data to dump can be worked out by looking at the header just above 0x4FC00.
+For all the maps I've looked at so far, it's 64 x 64 x 8 = 32768 (in decimal).
+
+The $length and $width variables in the script are also defaulted to 64 x 64 (again in decimal) but can be altered if the map size is different.
+
+example usage: php avpjag_mapreader.php > sublevel3.txt
+
+## Additional Notes
+Avp maps are stored as a 64 x 64 grid of cubes. Each cube is described by 8 bytes. 6 bytes to describe each face of the cube and 2 flags. 
+The co-ordinate system for the grid starts at 0,0 in the top left of the level, and the data is stored as rows running from left to right. 
+IE Sector 0,0 ... sector 63,0 then sector 0,1 ... sector 63,1 all the way through to sector 63,63
+
+
+This script reads these bytes and produces text codes in the following format
+aabbcdefgh
+aa = x co-ordinate of sector
+bb = y co-ordinate of sector
+c = left hand wall definition (assuming the player is facing 'north' towards the top of the grid)
+d = front wall definition
+e = right hand wall definition
+f = back wall definition
+g = ceiling definition
+h = floor definition
+
+each face can be defined as any of the following characters
+L standard left hand wall
+F standard front wall
+R standard right hand wall
+B standard back wall
+p pillars/wall struts on both sides
+l pillar/wall strut on left side only
+r pillar/wall strut on right side only
+d door
+x open space
+C ceiling
+F floor
+
---
+++ avpjag_mapreader.php
@@ -0,0 +1,184 @@
+<?php
+//Avp Jaguar map data interpreter script by Bobblen
+//based on reverse engineering by Rich Whitehouse and Bobblen (using tools also developed by Rich Whitehouse)
+//
+//map data can be dumped from RAM at offset 0x4FC00 using BigPEmu Debug version, this script only works with dumped map data
+//
+//this generates a text file to be used with the Blender python script avp_jaguar_map_builder.py
+//note that not all bytes have been reverse engineered yet, a wall will be assumed if the type is unknown. This is fine for most maps
+//
+//usage: php avpjag_mapreader.php > level.txt
+//don't forget to set your file name on line 18 and path on line 19!
+
+
+ini_set('memory_limit','128M');
+
+$type = 'textfile'; //currently only textfile
+
+$filename = 'SUBLEVEL3.BIN';
+$bin = fopen("D:\\Maps\\".$filename, "r");
+
+$length = 64; //get from header
+$width = 64; //get from header
+$totalsize = $length*$width;
+
+//debug
+//echo "AvP Jaguar map reader\n\n";
+//echo "file name is ".$filename."\n";
+//echo "file type is set to ".$type."\n";
+
+
+if( $type == 'textfile' ) {
+	
+	$county = -1; //we want Y coord to be set to 0 after first iteration, so start at -1
+			
+		for ($x = 0; $x <= ($totalsize-1); $x++) {
+		//get sector bytes
+		$side1 = getByte($bin);
+		$side2 = getByte($bin);
+		$side3 = getByte($bin);
+		$side4 = getByte($bin);
+		$ceiling = getByte($bin);
+		$floor = getByte($bin);
+		$leftdoorisopenflag = getByte($bin);
+		$frontdoorisopenflag = getByte($bin);
+		
+		//work out coordinates
+		$countx = $x%$length; //max x is 63, wrap back to 0 once we've iterated through all x values for the row
+		
+		if($countx==0)
+			{
+			$county = $county + 1; //increase y by 1 after each full row x values (also sets it to 0 on first interation)
+			}
+		
+		if (strlen($countx)==1)
+			{
+			$countx = '0'.$countx; //pad x to 2 digits
+			}
+			
+		if (strlen($county)==1)
+			{
+			$county = '0'.$county;  //pad y to 2 digits
+			}
+			
+		echo $countx.$county; //first print the coordinates of the current sector
+		
+		//now the codes for each face
+		if(!empty($side1)) //left wall
+			{
+			switch(true)
+				{
+					case($side1 == 32 || $side1 == 40 || $side1 == 48  || $side1 == 64 || $side1 == 80 || $side1 == 88):
+						echo "d"; //doors
+						break;
+					case($side1 == 104):
+						echo "l"; //pillar left
+						break;
+					case($side1 == 120):
+						echo "p"; //pillars both
+						break;
+					case($side1 == 232):
+						echo "r"; //pillar right
+						break;
+					default:
+						echo "L"; //normal left wall
+				}
+			}
+			else{echo "x";} //nothing
+			
+		if(!empty($side2)) //front wall
+			{
+			switch(true)
+				{
+					case($side2 == 32 || $side2 == 40 || $side2 == 48  || $side2 == 64 || $side2 == 80 || $side2 == 88):
+						echo "d"; //doors
+						break;
+					case($side2 == 104):
+						echo "l"; //pillar left
+						break;
+					case($side2 == 120):
+						echo "p"; //pillars both
+						break;
+					case($side2 == 232):
+						echo "r"; //pillar right
+						break;
+					default:
+						echo "F"; //normal front wall
+				}
+			}
+			else{echo "x";} //nothing
+			
+		if(!empty($side3)) //right wall
+			{
+			switch(true)
+				{
+					case($side3 == 32 || $side3 == 40 || $side3 == 48  || $side3 == 64 || $side3 == 80 || $side3 == 88):
+						echo "d"; //doors
+						break;
+					case($side3 == 104):
+						echo "l"; //pillar left
+						break;
+					case($side3 == 120):
+						echo "p"; //pillars both
+						break;
+					case($side3 == 232):
+						echo "r"; //pillar right
+						break;
+					default:
+						echo "R"; //normal right wall
+				}
+			}
+			else{echo "x";} //nothing
+			
+		if(!empty($side4)) //back wall
+			{
+			switch(true)
+				{
+					case($side4 == 32 || $side4 == 40 || $side4 == 48  || $side4 == 64 || $side4 == 80 || $side4 == 88):
+						echo "d"; //doors
+						break;
+					case($side4 == 104):
+						echo "l"; //pillar left
+						break;
+					case($side4 == 120):
+						echo "p"; //pillars both
+						break;
+					case($side4 == 232):
+						echo "r"; //pillar right
+						break;
+					default:
+						echo "B"; //normal back wall
+				}
+			}
+			else{echo "x";} //nothing
+			
+		if(!empty($ceiling))
+			{
+			echo "C"; //Ceiling
+			}
+			else{echo "x";} //nothing
+			
+		if(!empty($floor))
+			{
+			echo "F"; //Floor
+			}
+			else{echo "x";} //nothing
+			
+		echo "\n";
+		
+		}
+	
+	}
+
+
+////////////////////////////////////////////////////////
+
+// Functions
+function getByte( &$source )
+{
+	return ord( fread( $source,1 ) );
+}
+
+
+
+?>
